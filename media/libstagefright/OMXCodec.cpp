bd41345b7ac527df021a82a773103909c2c3d3d9
Andrew Boren <andrew.boren@gmail.com>
2014-05-25 18:00:48 -0700

    Revert "camera: Remove deprecated HTC camera support code"
    This reverts commit 0f4d10e7ea9aa431542a10d73fea372ca081e5c1.
    
    Change-Id: I75562a03d801e03e62f07d473c68d7f7bdc343d1

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -357,6 +357,10 @@ uint32_t OMXCodec::getComponentQuirks(
         quirks |= kSupportsMultipleFramesPerInputBuffer;
     }
     if (list->codecHasQuirk(
+                index, "requires-larger-encoder-output-buffer")) {
+        quirks |= kRequiresLargerEncoderOutputBuffer;
+    }
+    if (list->codecHasQuirk(
                 index, "requires-allocate-on-input-ports")) {
         quirks |= kRequiresAllocateBufferOnInputPorts;
     }
