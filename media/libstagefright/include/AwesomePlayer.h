d3348c8559fb32cc5ddfa94818aa758f77030cf7
Surajit Podder <spodder@codeaurora.org>
2014-09-17 19:23:28 +0530

    libstagefright: Add support to query selected track
    Added support for INVOKE_ID_GET_SELECTED_TRACK method
    in invoke to return the selected track in AwesomePlayer
    
    Change-Id: I955ee71befdd32c00db6500bf6fc5184a5b60f81
    CRs-Fixed: 721667

--- media/libstagefright/include/AwesomePlayer.h
+++ media/libstagefright/include/AwesomePlayer.h
@@ -174,6 +174,7 @@ private:
     bool mVideoRendererIsPreview;
     int32_t mMediaRenderingStartGeneration;
     int32_t mStartGeneration;
+    ssize_t mActiveVideoTrackIndex;
 
     ssize_t mActiveAudioTrackIndex;
     sp<MediaSource> mAudioTrack;
@@ -247,6 +248,7 @@ private:
 
     int64_t mLastVideoTimeUs;
     TimedTextDriver *mTextDriver;
+    ssize_t mActiveTextTrackIndex;
 
     sp<WVMExtractor> mWVMExtractor;
     sp<MediaExtractor> mExtractor;
