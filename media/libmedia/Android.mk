DolbyLaboratories/caf-platform-frameworks-av

audio: Audio resampler support for 192Khz playback
-Current AudioResampler limits input sampling rate to be
maximum of twice of output sampling rate to downsample.
The output sampling rate being 48khz, maximum input sampling
rate that be supported is 96khz.
Support is added to use qti resampler for playback upto 192khz.

Change-Id: Ia8f24a0874ebf6e16ef7bd1f2759a14f47149875

media/libmedia/Android.mk
@@ -73,6 +73,14 @@ LOCAL_SRC_FILES += SingleStateQueue.cpp
 LOCAL_CFLAGS += -DSINGLE_STATE_QUEUE_INSTANTIATIONS='"SingleStateQueueInstantiations.cpp"'
 # Consider a separate a library for SingleStateQueueInstantiations.
 
+#QTI Resampler
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(strip $(BOARD_USES_QCOM_RESAMPLER)),true)
+LOCAL_CFLAGS += -DQTI_RESAMPLER
+endif
+endif
+#QTI Resampler
+
 LOCAL_SHARED_LIBRARIES := \
 	libui liblog libcutils libutils libbinder libsonivox libicuuc libexpat \
         libcamera_client libstagefright_foundation \

services/audioflinger/AudioMixer.cpp
@@ -557,6 +557,11 @@ bool AudioMixer::track_t::setResampler(uint32_t value, uint32_t devSampleRate)
                 // FIXME this is flawed for dynamic sample rates, as we choose the resampler
                 // quality level based on the initial ratio, but that could change later.
                 // Should have a way to distinguish tracks with static ratios vs. dynamic ratios.
+#ifdef QTI_RESAMPLER
+                if (value > devSampleRate * 2) {
+                    quality = AudioResampler::QTI_QUALITY;
+                } else
+#endif
                 if (!((value == 44100 && devSampleRate == 48000) ||
                       (value == 48000 && devSampleRate == 44100))) {
                     quality = AudioResampler::LOW_QUALITY;

services/audioflinger/AudioResampler.cpp
@@ -26,6 +26,10 @@
 #include "AudioResamplerSinc.h"
 #include "AudioResamplerCubic.h"
 
+#ifdef QTI_RESAMPLER
+#include "AudioResamplerQTI.h"
+#endif
+
 #ifdef __arm__
 #include <machine/cpu-features.h>
 #endif
@@ -85,6 +89,9 @@ bool AudioResampler::qualityIsSupported(src_quality quality)
     case MED_QUALITY:
     case HIGH_QUALITY:
     case VERY_HIGH_QUALITY:
+#ifdef QTI_RESAMPLER
+    case QTI_QUALITY:
+#endif
         return true;
     default:
         return false;
@@ -105,7 +112,11 @@ void AudioResampler::init_routine()
         if (*endptr == '\0') {
             defaultQuality = (src_quality) l;
             ALOGD("forcing AudioResampler quality to %d", defaultQuality);
+#ifdef QTI_RESAMPLER
+            if (defaultQuality < DEFAULT_QUALITY || defaultQuality > QTI_QUALITY) {
+#else
             if (defaultQuality < DEFAULT_QUALITY || defaultQuality > VERY_HIGH_QUALITY) {
+#endif
                 defaultQuality = DEFAULT_QUALITY;
             }
         }
@@ -124,6 +135,9 @@ uint32_t AudioResampler::qualityMHz(src_quality quality)
     case HIGH_QUALITY:
         return 20;
     case VERY_HIGH_QUALITY:
+#ifdef QTI_RESAMPLER
+    case QTI_QUALITY: //for QTI_QUALITY, currently assuming same as VHQ
+#endif
         return 34;
     }
 }
@@ -175,6 +189,11 @@ AudioResampler* AudioResampler::create(int bitDepth, int inChannelCount,
         case VERY_HIGH_QUALITY:
             quality = HIGH_QUALITY;
             break;
+#ifdef QTI_RESAMPLER
+        case QTI_QUALITY:
+            quality = VERY_HIGH_QUALITY;
+            break;
+#endif
         }
     }
     pthread_mutex_unlock(&mutex);
@@ -200,6 +219,12 @@ AudioResampler* AudioResampler::create(int bitDepth, int inChannelCount,
         ALOGV("Create VERY_HIGH_QUALITY sinc Resampler = %d", quality);
         resampler = new AudioResamplerSinc(bitDepth, inChannelCount, sampleRate, quality);
         break;
+#ifdef QTI_RESAMPLER
+    case QTI_QUALITY:
+        ALOGV("Create QTI_QUALITY Resampler = %d",quality);
+        resampler = new AudioResamplerQTI(bitDepth, inChannelCount, sampleRate);
+        break;
+#endif
     }
 
     // initialize resampler

services/audioflinger/AudioResamplerQTI.cpp
@@ -1,0 +1,165 @@
+/*
+ * Copyright (C) 2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AudioResamplerQTI.h"
+#include "QCT_Resampler.h"
+#include <sys/time.h>
+
+namespace android {
+AudioResamplerQTI::AudioResamplerQTI(int bitDepth,
+        int inChannelCount, int32_t sampleRate)
+    :AudioResampler(bitDepth, inChannelCount, sampleRate, QTI_QUALITY),
+    mOutFrameCount(0), mTmpBuf(0), mFrameIndex(0)
+{
+    stateSize = QCT_Resampler::MemAlloc(bitDepth, inChannelCount, sampleRate, sampleRate);
+    mState = new int16_t[stateSize];
+    mVolume[0] = mVolume[1] = 0;
+    mBuffer.frameCount = 0;
+}
+
+AudioResamplerQTI::~AudioResamplerQTI()
+{
+    if (mState) {
+        delete [] mState;
+    }
+    if (mTmpBuf) {
+        delete [] mTmpBuf;
+    }
+}
+
+void AudioResamplerQTI::resample(int32_t* out, size_t outFrameCount,
+            AudioBufferProvider* provider)
+{
+    int16_t vl = mVolume[0];
+    int16_t vr = mVolume[1];
+    int16_t *pBuf;
+
+    size_t inFrameRequest;
+    size_t inFrameCount = getNumInSample(outFrameCount);
+    size_t index = 0;
+    size_t frameIndex = mFrameIndex;
+    size_t out_count = outFrameCount * 2;
+    if (mChannelCount == 1) {
+        inFrameRequest = inFrameCount;
+    } else {
+        inFrameRequest = inFrameCount * 2;
+    }
+
+    if (mOutFrameCount < outFrameCount) {
+        mOutFrameCount = outFrameCount;
+        if (mTmpBuf) {
+            delete [] mTmpBuf;
+        }
+        mTmpBuf = new int16_t[inFrameRequest + 16];
+    }
+
+    if (mChannelCount == 1) {
+        // buffer is empty, fetch a new one
+        while (index < inFrameCount) {
+            if (!mBuffer.frameCount) {
+                mBuffer.frameCount = inFrameCount;
+                provider->getNextBuffer(&mBuffer);
+                frameIndex = 0;
+            }
+
+            if (mBuffer.raw == NULL) {
+                while (index < inFrameCount) {
+                    mTmpBuf[index++] = 0;
+                }
+                QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+                goto resample_exit;
+            }
+
+            mTmpBuf[index++] = mBuffer.i16[frameIndex++];
+
+            if (frameIndex >= mBuffer.frameCount) {
+                provider->releaseBuffer(&mBuffer);
+            }
+        }
+
+        QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+    } else {
+        pBuf = &mTmpBuf[inFrameCount];
+        // buffer is empty, fetch a new one
+        while (index < inFrameCount) {
+            if (!mBuffer.frameCount) {
+                mBuffer.frameCount = inFrameCount;
+                provider->getNextBuffer(&mBuffer);
+                frameIndex = 0;
+            }
+            if (mBuffer.raw == NULL) {
+                while (index < inFrameCount) {
+                    mTmpBuf[index] = 0;
+                    pBuf[index++] = 0;
+                }
+                QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+                goto resample_exit;
+            }
+
+            mTmpBuf[index] = mBuffer.i16[frameIndex++];
+            pBuf[index++] =  mBuffer.i16[frameIndex++];
+
+            if (frameIndex >= mBuffer.frameCount * 2) {
+                provider->releaseBuffer(&mBuffer);
+            }
+       }
+
+       QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+    }
+
+    for (int i = 0; i < out_count; i += 2) {
+        out[i] *= vl;
+        out[i+1] *= vr;
+    }
+
+resample_exit:
+    mFrameIndex = frameIndex;
+}
+
+void AudioResamplerQTI::setSampleRate(int32_t inSampleRate)
+{
+    if (mInSampleRate != inSampleRate) {
+        mInSampleRate = inSampleRate;
+        init();
+    }
+}
+
+void AudioResamplerQTI::init()
+{
+    QCT_Resampler::Init(mState, mChannelCount, mInSampleRate, mSampleRate);
+}
+
+size_t AudioResamplerQTI::getNumInSample(size_t outFrameCount)
+{
+    size_t size = (size_t)QCT_Resampler::GetNumInSamp(mState, outFrameCount);
+    return size;
+}
+
+void AudioResamplerQTI::setVolume(int16_t left, int16_t right)
+{
+    mVolume[0] = left;
+    mVolume[1] = right;
+
+}
+
+void AudioResamplerQTI::reset()
+{
+    AudioResampler::reset();
+}
+
+}; // namespace android

services/audioflinger/AudioResamplerQTI.h
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <cutils/log.h>
+
+#include "AudioResampler.h"
+
+namespace android {
+// ----------------------------------------------------------------------------
+
+class AudioResamplerQTI : public AudioResampler {
+public:
+    AudioResamplerQTI(int bitDepth, int inChannelCount, int32_t sampleRate);
+    ~AudioResamplerQTI();
+    void resample(int32_t* out, size_t outFrameCount,
+                  AudioBufferProvider* provider);
+    void setSampleRate(int32_t inSampleRate);
+    size_t getNumInSample(size_t outFrameCount);
+
+    int16_t *mState;
+    int16_t *mTmpBuf;
+    size_t mFrameIndex;
+    size_t stateSize;
+    size_t mOutFrameCount;
+
+    static const int kNumTmpBufSize = 1024;
+
+    void init();
+    void setVolume(int16_t left, int16_t right);
+    void reset();
+};
+
+// ----------------------------------------------------------------------------
+}; // namespace android
+

Evervolv/android_frameworks_av

Conflicts:
	media/libmedia/AudioTrack.cpp
	services/audioflinger/Threads.cpp

media/libmedia/Android.mk
@@ -64,6 +64,15 @@ LOCAL_SRC_FILES:= \
 
 LOCAL_SRC_FILES += ../libnbaio/roundup.c
 
+
+#QTI Resampler
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTN_RESAMPLER)),true)
+LOCAL_CFLAGS += -DQTI_RESAMPLER
+endif
+endif
+#QTI Resampler
+
 LOCAL_SHARED_LIBRARIES := \
 	libui liblog libcutils libutils libbinder libsonivox libicuuc libicui18n libexpat \
         libcamera_client libstagefright_foundation \

services/audioflinger/AudioMixer.cpp
@@ -931,6 +931,11 @@ bool AudioMixer::track_t::setResampler(uint32_t trackSampleRate, uint32_t devSam
                 // FIXME this is flawed for dynamic sample rates, as we choose the resampler
                 // quality level based on the initial ratio, but that could change later.
                 // Should have a way to distinguish tracks with static ratios vs. dynamic ratios.
+#ifdef QTI_RESAMPLER
+                if (trackSampleRate > devSampleRate * 2) {
+                    quality = AudioResampler::QTI_QUALITY;
+                } else
+#endif
                 if (!((trackSampleRate == 44100 && devSampleRate == 48000) ||
                       (trackSampleRate == 48000 && devSampleRate == 44100))) {
                     quality = AudioResampler::DYN_LOW_QUALITY;

services/audioflinger/AudioResampler.cpp
@@ -28,6 +28,10 @@
 #include "AudioResamplerCubic.h"
 #include "AudioResamplerDyn.h"
 
+#ifdef QTI_RESAMPLER
+#include "AudioResamplerQTI.h"
+#endif
+
 #ifdef __arm__
 #include <machine/cpu-features.h>
 #endif
@@ -93,6 +97,9 @@ bool AudioResampler::qualityIsSupported(src_quality quality)
     case DYN_LOW_QUALITY:
     case DYN_MED_QUALITY:
     case DYN_HIGH_QUALITY:
+#ifdef QTI_RESAMPLER
+    case QTI_QUALITY:
+#endif
         return true;
     default:
         return false;
@@ -113,7 +120,11 @@ void AudioResampler::init_routine()
         if (*endptr == '\0') {
             defaultQuality = (src_quality) l;
             ALOGD("forcing AudioResampler quality to %d", defaultQuality);
+#ifdef QTI_RESAMPLER
+            if (defaultQuality < DEFAULT_QUALITY || defaultQuality > QTI_QUALITY) {
+#else
             if (defaultQuality < DEFAULT_QUALITY || defaultQuality > DYN_HIGH_QUALITY) {
+#endif
                 defaultQuality = DEFAULT_QUALITY;
             }
         }
@@ -132,6 +143,9 @@ uint32_t AudioResampler::qualityMHz(src_quality quality)
     case HIGH_QUALITY:
         return 20;
     case VERY_HIGH_QUALITY:
+#ifdef QTI_RESAMPLER
+    case QTI_QUALITY: //for QTI_QUALITY, currently assuming same as VHQ
+#endif
         return 34;
     case DYN_LOW_QUALITY:
         return 4;
@@ -207,6 +221,11 @@ AudioResampler* AudioResampler::create(audio_format_t format, int inChannelCount
         case DYN_HIGH_QUALITY:
             quality = DYN_MED_QUALITY;
             break;
+#ifdef QTI_RESAMPLER
+        case QTI_QUALITY:
+            quality = DYN_HIGH_QUALITY;
+            break;
+#endif
         }
     }
     pthread_mutex_unlock(&mutex);
@@ -253,6 +272,12 @@ AudioResampler* AudioResampler::create(audio_format_t format, int inChannelCount
             }
         }
         break;
+#ifdef QTI_RESAMPLER
+    case QTI_QUALITY:
+        ALOGV("Create QTI_QUALITY Resampler = %d",quality);
+        resampler = new AudioResamplerQTI(format, inChannelCount, sampleRate);
+        break;
+#endif
     }
 
     // initialize resampler

services/audioflinger/AudioResamplerQTI.cpp
@@ -1,0 +1,165 @@
+/*
+ * Copyright (C) 2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AudioResamplerQTI.h"
+#include "QCT_Resampler.h"
+#include <sys/time.h>
+
+namespace android {
+AudioResamplerQTI::AudioResamplerQTI(int format,
+        int inChannelCount, int32_t sampleRate)
+    :AudioResampler(inChannelCount, sampleRate, QTI_QUALITY),
+    mOutFrameCount(0), mTmpBuf(0), mFrameIndex(0)
+{
+    stateSize = QCT_Resampler::MemAlloc(format, inChannelCount, sampleRate, sampleRate);
+    mState = new int16_t[stateSize];
+    mVolume[0] = mVolume[1] = 0;
+    mBuffer.frameCount = 0;
+}
+
+AudioResamplerQTI::~AudioResamplerQTI()
+{
+    if (mState) {
+        delete [] mState;
+    }
+    if (mTmpBuf) {
+        delete [] mTmpBuf;
+    }
+}
+
+void AudioResamplerQTI::resample(int32_t* out, size_t outFrameCount,
+            AudioBufferProvider* provider)
+{
+    int16_t vl = mVolume[0];
+    int16_t vr = mVolume[1];
+    int16_t *pBuf;
+
+    size_t inFrameRequest;
+    size_t inFrameCount = getNumInSample(outFrameCount);
+    size_t index = 0;
+    size_t frameIndex = mFrameIndex;
+    size_t out_count = outFrameCount * 2;
+    if (mChannelCount == 1) {
+        inFrameRequest = inFrameCount;
+    } else {
+        inFrameRequest = inFrameCount * 2;
+    }
+
+    if (mOutFrameCount < outFrameCount) {
+        mOutFrameCount = outFrameCount;
+        if (mTmpBuf) {
+            delete [] mTmpBuf;
+        }
+        mTmpBuf = new int16_t[inFrameRequest + 16];
+    }
+
+    if (mChannelCount == 1) {
+        // buffer is empty, fetch a new one
+        while (index < inFrameCount) {
+            if (!mBuffer.frameCount) {
+                mBuffer.frameCount = inFrameCount;
+                provider->getNextBuffer(&mBuffer);
+                frameIndex = 0;
+            }
+
+            if (mBuffer.raw == NULL) {
+                while (index < inFrameCount) {
+                    mTmpBuf[index++] = 0;
+                }
+                QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+                goto resample_exit;
+            }
+
+            mTmpBuf[index++] = mBuffer.i16[frameIndex++];
+
+            if (frameIndex >= mBuffer.frameCount) {
+                provider->releaseBuffer(&mBuffer);
+            }
+        }
+
+        QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+    } else {
+        pBuf = &mTmpBuf[inFrameCount];
+        // buffer is empty, fetch a new one
+        while (index < inFrameCount) {
+            if (!mBuffer.frameCount) {
+                mBuffer.frameCount = inFrameCount;
+                provider->getNextBuffer(&mBuffer);
+                frameIndex = 0;
+            }
+            if (mBuffer.raw == NULL) {
+                while (index < inFrameCount) {
+                    mTmpBuf[index] = 0;
+                    pBuf[index++] = 0;
+                }
+                QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+                goto resample_exit;
+            }
+
+            mTmpBuf[index] = mBuffer.i16[frameIndex++];
+            pBuf[index++] =  mBuffer.i16[frameIndex++];
+
+            if (frameIndex >= mBuffer.frameCount * 2) {
+                provider->releaseBuffer(&mBuffer);
+            }
+       }
+
+       QCT_Resampler::Resample90dB(mState, mTmpBuf, out, inFrameCount, outFrameCount);
+    }
+
+    for (int i = 0; i < out_count; i += 2) {
+        out[i] *= vl;
+        out[i+1] *= vr;
+    }
+
+resample_exit:
+    mFrameIndex = frameIndex;
+}
+
+void AudioResamplerQTI::setSampleRate(int32_t inSampleRate)
+{
+    if (mInSampleRate != inSampleRate) {
+        mInSampleRate = inSampleRate;
+        init();
+    }
+}
+
+void AudioResamplerQTI::init()
+{
+    QCT_Resampler::Init(mState, mChannelCount, mInSampleRate, mSampleRate);
+}
+
+size_t AudioResamplerQTI::getNumInSample(size_t outFrameCount)
+{
+    size_t size = (size_t)QCT_Resampler::GetNumInSamp(mState, outFrameCount);
+    return size;
+}
+
+void AudioResamplerQTI::setVolume(int16_t left, int16_t right)
+{
+    mVolume[0] = left;
+    mVolume[1] = right;
+
+}
+
+void AudioResamplerQTI::reset()
+{
+    AudioResampler::reset();
+}
+
+}; // namespace android

services/audioflinger/AudioResamplerQTI.h
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2014, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <cutils/log.h>
+
+#include "AudioResampler.h"
+
+namespace android {
+// ----------------------------------------------------------------------------
+
+class AudioResamplerQTI : public AudioResampler {
+public:
+    AudioResamplerQTI(int format, int inChannelCount, int32_t sampleRate);
+    ~AudioResamplerQTI();
+    void resample(int32_t* out, size_t outFrameCount,
+                  AudioBufferProvider* provider);
+    void setSampleRate(int32_t inSampleRate);
+    size_t getNumInSample(size_t outFrameCount);
+
+    int16_t *mState;
+    int16_t *mTmpBuf;
+    size_t mFrameIndex;
+    size_t stateSize;
+    size_t mOutFrameCount;
+
+    static const int kNumTmpBufSize = 1024;
+
+    void init();
+    void setVolume(int16_t left, int16_t right);
+    void reset();
+};
+
+// ----------------------------------------------------------------------------
+}; // namespace android
+