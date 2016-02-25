d1e5e8bffb1aa44675a56910dd84c35b163f9873
Surajit Podder <spodder@codeaurora.org>
2016-02-25 20:23:24 +0530

    video: Use boot clock for recording start time
    Camera HAL3 uses boot time for buffer timestamp, rather
    than system monotonic time. This leads to issues as
    framework uses system monotonic time as reference start
    time for timestamp adjustment.
    
    Add change to use boot time for reference start time.
    
    CRs-Fixed: 946735
    
    Change-Id: Id0af9c8aed1a983095275ac03f7f59abc31594cc

--- media/libstagefright/MediaCodecSource.cpp
+++ media/libstagefright/MediaCodecSource.cpp
@@ -39,6 +39,7 @@
 #include <media/stagefright/Utils.h>
 #include <stagefright/AVExtensions.h>
 #include <OMX_Core.h>
+#include <cutils/properties.h>
 
 namespace android {
 
@@ -769,8 +770,15 @@ status_t MediaCodecSource::onStart(MetaData *params) {
     status_t err = OK;
 
     if (mFlags & FLAG_USE_SURFACE_INPUT) {
+        auto key = kKeyTimeBoot;
+        char value[PROPERTY_VALUE_MAX];
+        if (property_get("media.camera.ts.monotonic", value, "0") &&
+            atoi(value)) {
+            key = kKeyTime;
+        }
+
         int64_t startTimeUs;
-        if (!params || !params->findInt64(kKeyTime, &startTimeUs)) {
+        if (!params || !params->findInt64(key, &startTimeUs)) {
             startTimeUs = -1ll;
         }
         resume(startTimeUs);
@@ -878,6 +886,7 @@ void MediaCodecSource::onMessageReceived(const sp<AMessage> &msg) {
             MediaBuffer *mbuf = new MediaBuffer(outbuf->size());
             mbuf->setObserver(this);
             mbuf->add_ref();
+
             sp<MetaData> meta = mbuf->meta_data();
             AVUtils::get()->setDeferRelease(meta);
 
