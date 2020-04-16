73529daf16fcfa38d96e3e81275094cfc05c9860
myfluxi <myfluxi@users.noreply.github.com>
2020-04-16 12:53:23 +0000

    audioflinger: Fix audio for WifiDisplay
    AudioFlinger is not able to determine the correct
    pid/tid for WifiDisplay and thus we do not pass checks
    for CAPTURE_AUDIO_OUTPUT and RECORD_AUDIO permissions.
    
    To fix audio for WifiDisplay, it should be safe to
    always allow a trusted calling uid (AID_MEDIA which
    has the same perms as AID_AUDIOSERVER).
    
    Change-Id: Ifa46d8e77a43027645cad02a04263b58e134c3ad

--- services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
+++ services/audiopolicy/service/AudioPolicyInterfaceImpl.cpp
@@ -702,7 +702,8 @@ Status AudioPolicyService::getInputForAttr(const media::audio::common::AudioAttr
     const auto isRecordingAllowed = audioserver_permissions() ?
             CHECK_PERM(RECORD_AUDIO, attributionSource.uid) :
             recordingAllowed(attributionSource, inputSource);
-    if (!(isRecordingAllowed
+    if (!isAudioServerOrMediaServerUid(attributionSource.uid)
+            && !(isRecordingAllowed
             || inputSource == AUDIO_SOURCE_FM_TUNER
             || inputSource == AUDIO_SOURCE_REMOTE_SUBMIX
             || inputSource == AUDIO_SOURCE_ECHO_REFERENCE)) {
@@ -796,7 +797,7 @@ Status AudioPolicyService::getInputForAttr(const media::audio::common::AudioAttr
                 // FIXME: use the same permission as for remote submix for now.
                 FALLTHROUGH_INTENDED;
             case AudioPolicyInterface::API_INPUT_MIX_CAPTURE:
-                if (!canCaptureOutput) {
+                if (!isAudioServerOrMediaServerUid(attributionSource.uid) && !canCaptureOutput) {
                     ALOGE("%s permission denied: capture not allowed", __func__);
                     status = PERMISSION_DENIED;
                 }
@@ -909,10 +910,10 @@ Status AudioPolicyService::startInput(int32_t portIdAidl)
     const auto permitted = startRecording(client->attributionSource, client->virtualDeviceId,
             String16(msg.str().c_str()), client->attributes.source);
 
-    // check calling permissions
-    if (permitted == PERMISSION_HARD_DENIED && client->attributes.source != AUDIO_SOURCE_FM_TUNER
+    if (!isAudioServerOrMediaServerUid(client->attributionSource.uid)
+            && (permitted == PERMISSION_HARD_DENIED && client->attributes.source != AUDIO_SOURCE_FM_TUNER
             && client->attributes.source != AUDIO_SOURCE_REMOTE_SUBMIX
-            && client->attributes.source != AUDIO_SOURCE_ECHO_REFERENCE) {
+            && client->attributes.source != AUDIO_SOURCE_ECHO_REFERENCE)) {
         ALOGE("%s permission denied: recording not allowed for attribution source %s",
                 __func__, client->attributionSource.toString().c_str());
         return binderStatusFromStatusT(PERMISSION_DENIED);
